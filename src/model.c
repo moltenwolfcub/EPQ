df7022e56e7b51b176187c0ce7cd80b2ed47f1c5
MoltenWolfCub <oliver@tomlins.org.uk>
2025-05-31 19:36:13 +0100

    cleaned up C code
    removed debugging prints and cleaned up unimplemented functions

--- src/model.c
+++ src/model.c
@@ -12,9 +12,6 @@ typedef struct
 
 static size_t MyReadProc(C_STRUCT aiFile *f, char *buffer, size_t size, size_t count)
 {
-	printf("READ\n");
-	printf("\treading: size=%d, count=%d\n", size, count);
-
 	EmbeddedData *embed = (EmbeddedData *)f->UserData;
 	if (!embed || !buffer || size == 0 || count == 0)
 		return 0;
@@ -28,31 +25,15 @@ static size_t MyReadProc(C_STRUCT aiFile *f, char *buffer, size_t size, size_t c
 
 	return bytes_to_read / size;
 }
-static size_t MyWriteProc(C_STRUCT aiFile *f, const char *, size_t, size_t)
-{
-	printf("WRITE\n");
-	return 0;
-}
-static size_t MyTellProc(C_STRUCT aiFile *f)
-{
-	printf("TELL\n");
-	return 0;
-}
 static size_t MyFileSizeProc(C_STRUCT aiFile *f)
 {
-	printf("FILE_SIZE\n");
-
 	EmbeddedData *embed = (EmbeddedData *)f->UserData;
 	if (!embed)
 		return 0;
-	printf("\tfile-size: %d\n", embed->size);
 	return embed->size;
 }
 static C_ENUM aiReturn MySeekProc(C_STRUCT aiFile *f, size_t offset, C_ENUM aiOrigin origin)
 {
-	printf("SEEK\n");
-	printf("\tsought: offset=%zu, origin=%d\n", offset, origin);
-
 	EmbeddedData *embed = (EmbeddedData *)f->UserData;
 	if (!embed)
 		return aiReturn_FAILURE;
@@ -81,16 +62,12 @@ static C_ENUM aiReturn MySeekProc(C_STRUCT aiFile *f, size_t offset, C_ENUM aiOr
 	embed->offset = new_offset;
 	return aiReturn_FAILURE;
 }
-static void MyFlushProc(C_STRUCT aiFile *f)
-{
-	printf("FLUSH\n");
-}
+static void MyFlushProc(C_STRUCT aiFile *) { printf("FLUSH UNIMPLEMENTED\n"); }
+static size_t MyWriteProc(C_STRUCT aiFile *, const char *, size_t, size_t) { return printf("WRITE UNIMPLEMENTED\n") * 0; }
+static size_t MyTellProc(C_STRUCT aiFile *) { return printf("TELL UNIMPLEMENTED\n") * 0; }
 
 static C_STRUCT aiFile *MyOpenProc(C_STRUCT aiFileIO *io, const char *filename, const char *mode)
 {
-	printf("OPEN\n");
-	printf("\topened: %s in %s\n", filename, mode);
-
 	int size = 0;
 	char *data = GetRawModel((char *)filename, &size);
 
@@ -109,11 +86,8 @@ static C_STRUCT aiFile *MyOpenProc(C_STRUCT aiFileIO *io, const char *filename, 
 	file->UserData = embed;
 	return file;
 }
-
 static void MyCloseProc(C_STRUCT aiFileIO *io, C_STRUCT aiFile *file)
 {
-	printf("CLOSE\n");
-
 	EmbeddedData *embed = (EmbeddedData *)file->UserData;
 	if (embed)
 	{
@@ -126,70 +100,9 @@ static void MyCloseProc(C_STRUCT aiFileIO *io, C_STRUCT aiFile *file)
 
 C_STRUCT aiFileIO *CreateMemoryFileIO()
 {
-	printf("CREATE\n");
-
 	C_STRUCT aiFileIO *io = (C_STRUCT aiFileIO *)malloc(sizeof(C_STRUCT aiFileIO));
 	io->OpenProc = MyOpenProc;
 	io->CloseProc = MyCloseProc;
 	io->UserData = NULL;
 	return io;
 }
-
-// aiFile callbacks
-// typedef size_t          (*aiFileWriteProc) (C_STRUCT aiFile*,   const char*, size_t, size_t);
-// typedef size_t          (*aiFileReadProc)  (C_STRUCT aiFile*,   char*, size_t,size_t);
-// typedef size_t          (*aiFileTellProc)  (C_STRUCT aiFile*);
-// typedef void            (*aiFileFlushProc) (C_STRUCT aiFile*);
-// typedef C_ENUM aiReturn (*aiFileSeek)      (C_STRUCT aiFile*, size_t, C_ENUM aiOrigin);
-
-// aiFileIO callbacks
-// typedef C_STRUCT aiFile* (*aiFileOpenProc)  (C_STRUCT aiFileIO*, const char*, const char*);
-// typedef void             (*aiFileCloseProc) (C_STRUCT aiFileIO*, C_STRUCT aiFile*);
-
-// Represents user-defined data
-// typedef char* aiUserData;
-
-// struct aiFileIO
-// {
-//     /** Function used to open a new file
-//      */
-//     aiFileOpenProc OpenProc;
-
-//     /** Function used to close an existing file
-//      */
-//     aiFileCloseProc CloseProc;
-
-//     /** User-defined, opaque data */
-//     aiUserData UserData;
-// };
-
-// struct aiFile {
-//     /** Callback to read from a file */
-//     aiFileReadProc ReadProc;
-
-//     /** Callback to write to a file */
-//     aiFileWriteProc WriteProc;
-
-//     /** Callback to retrieve the current position of
-//      *  the file cursor (ftell())
-//      */
-//     aiFileTellProc TellProc;
-
-//     /** Callback to retrieve the size of the file,
-//      *  in bytes
-//      */
-//     aiFileTellProc FileSizeProc;
-
-//     /** Callback to set the current position
-//      * of the file cursor (fseek())
-//      */
-//     aiFileSeek SeekProc;
-
-//     /** Callback to flush the file contents
-//      */
-//     aiFileFlushProc FlushProc;
-
-//     /** User-defined, opaque data
-//      */
-//     aiUserData UserData;
-// };
