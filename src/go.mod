8f9c965ca1545718e5171e55a5a8cc727e51d85d
MoltenWolfCub <oliver@tomlins.org.uk>
2025-07-04 15:25:47 +0100

    started implementing tests
    some basic tests to ensure that the model loading
    works properly. Many more to come.

--- src/go.mod
+++ src/go.mod
@@ -5,5 +5,6 @@ go 1.24.4
 require (
 	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71
 	github.com/go-gl/mathgl v1.2.0
+	github.com/google/go-cmp v0.7.0
 	github.com/veandco/go-sdl2 v0.4.40
 )
---
+++ src/model_test/basicCube_test.go
@@ -0,0 +1,72 @@
+package model_test
+
+import (
+	"testing"
+
+	"github.com/go-gl/mathgl/mgl32"
+	"github.com/google/go-cmp/cmp"
+	"github.com/moltenwolfcub/EPQ/src/model"
+)
+
+func TestBCMeshCount(t *testing.T) {
+	m := model.NewModel("testdata/basicCube.obj", false)
+
+	if len(m.Meshes) != 1 {
+		t.Errorf("NewModel(basicCube): len(meshes). Got %d meshes. Want 1.", len(m.Meshes))
+	}
+}
+
+func TestBCMeshVertexCount(t *testing.T) {
+	m := model.NewModel("testdata/basicCube.obj", false)
+	mesh := m.Meshes[0]
+
+	if len(mesh.Vertices) != 6*4 {
+		t.Errorf("NewModel(basicCube): len(mesh.vertices). Got %d vertices. Want %d.", len(mesh.Vertices), 6*4)
+	}
+}
+
+func TestBCMeshVertexPositions(t *testing.T) {
+	m := model.NewModel("testdata/basicCube.obj", false)
+	mesh := m.Meshes[0]
+
+	positions := make([]mgl32.Vec3, 0, len(mesh.Vertices))
+	for _, vertex := range mesh.Vertices {
+		positions = append(positions, vertex.Position)
+	}
+
+	want := []mgl32.Vec3{
+		{1, 1, -1},
+		{-1, 1, -1},
+		{-1, 1, 1},
+		{1, 1, 1},
+
+		{1, -1, 1},
+		{1, 1, 1},
+		{-1, 1, 1},
+		{-1, -1, 1},
+
+		{-1, -1, 1},
+		{-1, 1, 1},
+		{-1, 1, -1},
+		{-1, -1, -1},
+
+		{-1, -1, -1},
+		{1, -1, -1},
+		{1, -1, 1},
+		{-1, -1, 1},
+
+		{1, -1, -1},
+		{1, 1, -1},
+		{1, 1, 1},
+		{1, -1, 1},
+
+		{-1, -1, -1},
+		{-1, 1, -1},
+		{1, 1, -1},
+		{1, -1, -1},
+	}
+
+	if !cmp.Equal(positions, want) {
+		t.Errorf("NewModel(basicCube): mesh.vertices.positions. Got %v. Want %v.", positions, want)
+	}
+}
