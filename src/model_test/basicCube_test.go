ee7e164946ebe0016464159d20db225aba61a8a8
MoltenWolfCub <oliver@tomlins.org.uk>
2025-07-05 13:18:36 +0100

    added some more tests of for Vertex
    ensured that []vertex was loaded correctly
    
    normals and texCoords

--- src/model_test/basicCube_test.go
+++ src/model_test/basicCube_test.go
@@ -70,3 +70,96 @@ func TestBCMeshVertexPositions(t *testing.T) {
 		t.Errorf("NewModel(basicCube): mesh.vertices.positions. Got %v. Want %v.", positions, want)
 	}
 }
+
+func TestBCMeshVertexNormals(t *testing.T) {
+	m := model.NewModel("testdata/basicCube.obj", false)
+	mesh := m.Meshes[0]
+
+	normals := make([]mgl32.Vec3, 0, len(mesh.Vertices))
+	for _, vertex := range mesh.Vertices {
+		normals = append(normals, vertex.Normal)
+	}
+
+	want := []mgl32.Vec3{
+		{0, 1, 0},
+		{0, 1, 0},
+		{0, 1, 0},
+		{0, 1, 0},
+
+		{0, 0, 1},
+		{0, 0, 1},
+		{0, 0, 1},
+		{0, 0, 1},
+
+		{-1, 0, 0},
+		{-1, 0, 0},
+		{-1, 0, 0},
+		{-1, 0, 0},
+
+		{0, -1, 0},
+		{0, -1, 0},
+		{0, -1, 0},
+		{0, -1, 0},
+
+		{1, 0, 0},
+		{1, 0, 0},
+		{1, 0, 0},
+		{1, 0, 0},
+
+		{0, 0, -1},
+		{0, 0, -1},
+		{0, 0, -1},
+		{0, 0, -1},
+	}
+
+	if !cmp.Equal(normals, want) {
+		t.Errorf("NewModel(basicCube): mesh.vertices.normals. Got %v. Want %v.", normals, want)
+	}
+}
+
+func TestBCMeshVertexTexCoords(t *testing.T) {
+	m := model.NewModel("testdata/basicCube.obj", false)
+	mesh := m.Meshes[0]
+
+	texCoords := make([]mgl32.Vec2, 0, len(mesh.Vertices))
+	for _, vertex := range mesh.Vertices {
+		texCoords = append(texCoords, vertex.TexCoords)
+	}
+
+	// 1-Ycoord because of the flipUVs flag
+	want := []mgl32.Vec2{
+		{0.625, 1 - 0.50},
+		{0.875, 1 - 0.50},
+		{0.875, 1 - 0.75},
+		{0.625, 1 - 0.75},
+
+		{0.375, 1 - 0.75},
+		{0.625, 1 - 0.75},
+		{0.625, 1 - 1.00},
+		{0.375, 1 - 1.00},
+
+		{0.375, 1 - 0.00},
+		{0.625, 1 - 0.00},
+		{0.625, 1 - 0.25},
+		{0.375, 1 - 0.25},
+
+		{0.125, 1 - 0.50},
+		{0.375, 1 - 0.50},
+		{0.375, 1 - 0.75},
+		{0.125, 1 - 0.75},
+
+		{0.375, 1 - 0.50},
+		{0.625, 1 - 0.50},
+		{0.625, 1 - 0.75},
+		{0.375, 1 - 0.75},
+
+		{0.375, 1 - 0.25},
+		{0.625, 1 - 0.25},
+		{0.625, 1 - 0.50},
+		{0.375, 1 - 0.50},
+	}
+
+	if !cmp.Equal(texCoords, want) {
+		t.Errorf("NewModel(basicCube): mesh.vertices.normals. Got %v. Want %v.", texCoords, want)
+	}
+}
